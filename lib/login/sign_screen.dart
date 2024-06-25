9a6cc8793175308dad3dd8caf2b1686acd1b5205
Ahmed-Ibrahim618 <ahmed.ibrahim61879@gmail.com>
2024-06-25 16:33:16 +0300

    hhh

--- lib/login/sign_screen.dart
+++ lib/login/sign_screen.dart
@@ -1,8 +1,24 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_application_1/constants/colors.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 
-class SignScreen extends StatelessWidget {
+class SignScreen extends StatefulWidget {
   const SignScreen({super.key});
+
+  @override
+  _SignScreenState createState() => _SignScreenState();
+}
+
+class _SignScreenState extends State<SignScreen> {
+  bool _passwordVisible = false;
+
+  @override
+  void initState() {
+    super.initState();
+    _passwordVisible = false;
+  }
+
+  @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
@@ -12,98 +28,181 @@ class SignScreen extends StatelessWidget {
         ),
         elevation: 0,
         leading: IconButton(
-          icon: Icon(Icons.arrow_back, color: Colors.red),
+          icon: const Icon(Icons.arrow_back, color: redcolor),
           onPressed: () {
             Navigator.of(context).pop();
           },
         ),
       ),
-      body: Padding(
-        padding: const EdgeInsets.symmetric(horizontal: 20.0),
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: [
-            Text(
-              'أهلا بك معنا',
-              style: TextStyle(
-                fontSize: 24,
-                color: Colors.grey,
-              ),
-            ),
-            SizedBox(height: 30),
-            TextField(
-              decoration: InputDecoration(
-                labelText: 'الاسم',
-                border: UnderlineInputBorder(),
-              ),
-            ),
-            TextField(
-              decoration: InputDecoration(
-                labelText: 'الإيميل',
-                border: UnderlineInputBorder(),
-              ),
-            ),
-            TextField(
-              obscureText: true,
-              decoration: InputDecoration(
-                labelText: 'الرقم السري',
-                border: UnderlineInputBorder(),
-                suffixIcon: Icon(Icons.visibility_off),
-              ),
-            ),
-            SizedBox(height: 20),
-            Center(
-              child: ElevatedButton(
-                onPressed: () {},
-                style: ElevatedButton.styleFrom(
-                  backgroundColor: Colors.red,
-                  padding: EdgeInsets.symmetric(horizontal: 50, vertical: 15),
-                  textStyle: TextStyle(fontSize: 18),
+      body: Directionality(
+        textDirection: TextDirection.rtl,
+        child: SingleChildScrollView(
+          padding: const EdgeInsets.all(20.0),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              const Text(
+                'أهلا بك معنا',
+                style: TextStyle(
+                  fontSize: 24,
+                  color: Colors.grey,
                 ),
-                child: Text('إنشاء حساب'),
               ),
-            ),
-            SizedBox(height: 20),
-            Center(
-              child: Text(
-                'أو',
-                style: TextStyle(fontSize: 18, color: Colors.grey),
+              const SizedBox(height: 30),
+              const TextField(
+                decoration: InputDecoration(
+                  labelText: 'الاسم',
+                  prefixIcon: Icon(Icons.person),
+                  border: UnderlineInputBorder(),
+                  focusedBorder: UnderlineInputBorder(
+                    borderSide: BorderSide(color: redcolor),
+                  ),
+                  enabledBorder: UnderlineInputBorder(
+                    borderSide: BorderSide(color: redcolor),
+                  ),
+                ),
               ),
-            ),
-            SizedBox(height: 20),
-            Row(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-                IconButton(
-                  icon: Icon(Icons.facebook, color: Colors.red, size: 30),
+              const SizedBox(height: 15),
+              const TextField(
+                decoration: InputDecoration(
+                  labelText: 'الايميل',
+                  prefixIcon: Icon(Icons.email),
+                  border: UnderlineInputBorder(),
+                  focusedBorder: UnderlineInputBorder(
+                    borderSide: BorderSide(color: redcolor),
+                  ),
+                  enabledBorder: UnderlineInputBorder(
+                    borderSide: BorderSide(color: redcolor),
+                  ),
+                ),
+              ),
+              const SizedBox(height: 15),
+              TextField(
+                obscureText: !_passwordVisible,
+                decoration: InputDecoration(
+                  labelText: 'الرقم السري',
+                  prefixIcon: const Icon(Icons.lock),
+                  suffixIcon: IconButton(
+                    icon: Icon(
+                      _passwordVisible
+                          ? Icons.visibility
+                          : Icons.visibility_off,
+                    ),
+                    onPressed: () {
+                      setState(() {
+                        _passwordVisible = !_passwordVisible;
+                      });
+                    },
+                  ),
+                  border: const UnderlineInputBorder(),
+                  focusedBorder: const UnderlineInputBorder(
+                    borderSide: BorderSide(color: redcolor),
+                  ),
+                  enabledBorder: const UnderlineInputBorder(
+                    borderSide: BorderSide(color: redcolor),
+                  ),
+                ),
+              ),
+              const SizedBox(height: 15),
+              // ignore: prefer_const_constructors
+              TextField(
+                decoration: const InputDecoration(
+                  labelText: 'اسم ولي الامر',
+                  prefixIcon: Icon(Icons.person_outline),
+                  border: UnderlineInputBorder(),
+                  focusedBorder: UnderlineInputBorder(
+                    borderSide: BorderSide(color: redcolor),
+                  ),
+                  enabledBorder: UnderlineInputBorder(
+                    borderSide: BorderSide(color: redcolor),
+                  ),
+                ),
+              ),
+              const SizedBox(height: 15),
+              const TextField(
+                decoration: InputDecoration(
+                  labelText: 'رقم ولي الامر',
+                  prefixIcon: Icon(Icons.phone),
+                  border: UnderlineInputBorder(),
+                  focusedBorder: UnderlineInputBorder(
+                    borderSide: BorderSide(color: redcolor),
+                  ),
+                  enabledBorder: UnderlineInputBorder(
+                    borderSide: BorderSide(color: redcolor),
+                  ),
+                ),
+              ),
+              const SizedBox(height: 30),
+              Center(
+                child: ElevatedButton(
                   onPressed: () {},
-                ),
-                SizedBox(width: 20),
-                IconButton(
-                  icon: Icon(Icons.camera_alt,
-                      color: Colors.red, size: 30), // Assuming Instagram icon
-                  onPressed: () {},
-                ),
-                SizedBox(width: 20),
-                IconButton(
-                  icon: Icon(Icons.clear,
-                      color: Colors.red, size: 30), // Assuming X icon
-                  onPressed: () {},
-                ),
-              ],
-            ),
-            Spacer(),
-            Center(
-              child: TextButton(
-                onPressed: () {},
-                child: Text(
-                  'لديك حساب؟ تسجيل الدخول',
-                  style: TextStyle(fontSize: 18, color: Colors.red),
+                  style: ElevatedButton.styleFrom(
+                    backgroundColor: redcolor,
+                    padding: const EdgeInsets.symmetric(
+                        horizontal: 50, vertical: 15),
+                    textStyle: const TextStyle(fontSize: 18),
+                    shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(10),
+                    ),
+                  ),
+                  child: const Text(
+                    'اكمل',
+                    style: TextStyle(color: Colors.white),
+                  ),
                 ),
               ),
-            ),
-            SizedBox(height: 20),
-          ],
+              const SizedBox(height: 30),
+              const Row(
+                children: [
+                  Expanded(child: Divider(color: Colors.grey)),
+                  Padding(
+                    padding: EdgeInsets.symmetric(horizontal: 10.0),
+                    child: Text('أو',
+                        style: TextStyle(fontSize: 18, color: Colors.grey)),
+                  ),
+                  Expanded(child: Divider(color: Colors.grey)),
+                ],
+              ),
+              const SizedBox(height: 30),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  IconButton(
+                    icon: const FaIcon(FontAwesomeIcons.facebook,
+                        color: redcolor),
+                    iconSize: 30,
+                    onPressed: () {},
+                  ),
+                  const SizedBox(width: 20),
+                  IconButton(
+                    icon: const FaIcon(FontAwesomeIcons.instagram,
+                        color: redcolor),
+                    iconSize: 30,
+                    onPressed: () {},
+                  ),
+                  const SizedBox(width: 20),
+                  IconButton(
+                    icon:
+                        const FaIcon(FontAwesomeIcons.twitter, color: redcolor),
+                    iconSize: 30,
+                    onPressed: () {},
+                  ),
+                ],
+              ),
+              const SizedBox(height: 30),
+              Center(
+                child: TextButton(
+                  onPressed: () {
+                    Navigator.of(context).pop();
+                  },
+                  child: const Text(
+                    'لديك حساب؟ تسجيل الدخول',
+                    style: TextStyle(fontSize: 18, color: redcolor),
+                  ),
+                ),
+              ),
+            ],
+          ),
         ),
       ),
     );
