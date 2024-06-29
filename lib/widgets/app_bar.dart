b9ddd43a3a37a534a1874512744afe4e80b26eca
yousseftamer2003 <yousseftamer2003.yt@gmail.com>
2024-06-29 11:04:43 +0300

    curriculum

--- lib/widgets/app_bar.dart
+++
@@ -1,9 +0,0 @@
-import 'package:flutter/material.dart';
-import 'package:flutter_application_1/constants/colors.dart';
-
-AppBar buildAppBar(){
-  return AppBar(
-      backgroundColor: redcolor,
-      title: const Text('اهلا بك محمد',style: TextStyle(color: redcolor),),
-    );
-}
--- lib/widgets/curriculum_grid.dart
+++ lib/widgets/curriculum_grid.dart
@@ -1,30 +1,23 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_application_1/constants/colors.dart';
-import 'package:flutter_application_1/screens/curriculums_screen.dart';
 
-class HomeGrid extends StatelessWidget {
-  const HomeGrid({super.key});
+class CurriculumGrid extends StatelessWidget {
+  const CurriculumGrid({super.key});
 
   @override
   Widget build(BuildContext context) {
-    List<String> texts = ['واجبات','مناهج','حصص لايف','مراجعات الشهور','حل امتحانات','مراجعة نهائية'];
-    List<String> images = ['assets/images/Frame.png','assets/images/Frame (1).png','assets/images/ICON.png','assets/images/Frame (2).png','assets/images/Layer_1.png','assets/images/Group.png'];
-    return GridView.builder(
+    List<String> texts = ['رياضيات','عربي','دراسات','علوم','اللغة الفرنسية','اللغة الانجليزية','التكنولوجيا','المهارات'];
+    List<String> images = ['assets/images/1.png','assets/images/2.png','assets/images/3.png','assets/images/4.png','assets/images/5.png','assets/images/6.png','assets/images/7.png','assets/images/8.png'];
+    return  GridView.builder(
       gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
         crossAxisCount: 2,
         mainAxisSpacing: 20,
         crossAxisSpacing: 20,
         ),
-        itemCount: texts.length,
+      itemCount: texts.length,
       itemBuilder: (context, index) {
         return GestureDetector(
-          onTap: (){
-            if(texts[index] == 'مناهج'){
-              Navigator.of(context).push(
-                MaterialPageRoute(builder: (ctx)=> const CurriculumsScreen())
-              );
-            }
-          },
+          onTap: (){},
           child: Card(
             color: Colors.white,
             elevation: 3,
