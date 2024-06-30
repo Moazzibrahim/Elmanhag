d2a525634916c0b2844b30b34c6ed326f77ede8a
Ahmed-Ibrahim618 <ahmed.ibrahim61879@gmail.com>
2024-06-30 11:12:00 +0300

    hh

--- lib/views/screens/login/sign_screen.dart
+++ lib/views/screens/login/sign_screen.dart
@@ -7,20 +7,24 @@ class SignScreen extends StatelessWidget {
   final TextEditingController _nameController = TextEditingController();
   final TextEditingController _emailController = TextEditingController();
   final TextEditingController _passwordController = TextEditingController();
+  final TextEditingController _confirmPasswordController =
+      TextEditingController();
   final TextEditingController _parentNameController = TextEditingController();
   final TextEditingController _parentPhoneController = TextEditingController();
+
+  SignScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text(
+        title: const Text(
           'انشاء حساب',
           style: TextStyle(fontWeight: FontWeight.bold, color: redcolor),
         ),
         elevation: 0,
         leading: IconButton(
-          icon: Icon(Icons.arrow_back, color: redcolor),
+          icon: const Icon(Icons.arrow_back, color: redcolor),
           onPressed: () {
             Navigator.of(context).pop();
           },
@@ -29,94 +33,124 @@ class SignScreen extends StatelessWidget {
       body: Directionality(
         textDirection: TextDirection.rtl,
         child: SingleChildScrollView(
-          padding: EdgeInsets.all(20.0),
+          padding: const EdgeInsets.all(20.0),
           child: Column(
             crossAxisAlignment: CrossAxisAlignment.start,
             children: [
-              Text(
+              const Text(
                 'أهلا بك معنا',
                 style: TextStyle(
                   fontSize: 24,
                   color: Colors.grey,
                 ),
               ),
-              SizedBox(height: 30),
+              const SizedBox(height: 30),
               buildTextField(
                 controller: _nameController,
                 labelText: 'الاسم',
-                prefixIcon: Icon(Icons.person, color: redcolor),
+                prefixIcon: const Icon(Icons.person, color: redcolor),
               ),
-              SizedBox(height: 15),
+              const SizedBox(height: 15),
               buildTextField(
                 controller: _emailController,
                 labelText: 'الايميل',
-                prefixIcon: Icon(Icons.email, color: redcolor),
+                prefixIcon: const Icon(Icons.email, color: redcolor),
               ),
-              SizedBox(height: 15),
-              buildPasswordField(),
-              SizedBox(height: 15),
+              const SizedBox(height: 15),
+              buildPasswordField(_passwordController, 'الرقم السري'),
+              const SizedBox(height: 15),
+              buildPasswordField(
+                  _confirmPasswordController, 'تأكيد الرقم السري'),
+              const SizedBox(height: 15),
               buildTextField(
                 controller: _parentNameController,
                 labelText: 'اسم ولي الامر',
-                prefixIcon: Icon(Icons.person_outline, color: redcolor),
+                prefixIcon: const Icon(Icons.person_outline, color: redcolor),
               ),
-              SizedBox(height: 15),
+              const SizedBox(height: 15),
               buildTextField(
                 controller: _parentPhoneController,
                 labelText: 'رقم ولي الامر',
-                prefixIcon: Icon(Icons.phone, color: redcolor),
+                prefixIcon: const Icon(Icons.phone, color: redcolor),
               ),
-              SizedBox(height: 30),
+              const SizedBox(height: 30),
               Center(
                 child: ElevatedButton(
                   onPressed: () {
-                    Navigator.push(
-                      context,
-                      MaterialPageRoute(
-                        builder: (context) => SecondSignScreen(
-                          name: _nameController.text,
-                          email: _emailController.text,
-                          password: _passwordController.text,
-                          parentName: _parentNameController.text,
-                          parentPhone: _parentPhoneController.text,
+                    if (_nameController.text.isEmpty ||
+                        _emailController.text.isEmpty ||
+                        _passwordController.text.isEmpty ||
+                        _confirmPasswordController.text.isEmpty ||
+                        _parentNameController.text.isEmpty ||
+                        _parentPhoneController.text.isEmpty) {
+                      // Show an error message
+                      ScaffoldMessenger.of(context).showSnackBar(
+                        const SnackBar(
+                          content: Text('يجب ملء جميع الحقول'),
+                          backgroundColor: Colors.red,
                         ),
-                      ),
-                    );
+                      );
+                    } else if (_passwordController.text !=
+                        _confirmPasswordController.text) {
+                      // Show an error message
+                      ScaffoldMessenger.of(context).showSnackBar(
+                        const SnackBar(
+                          content: Text('كلمتا السر غير متطابقتين'),
+                          backgroundColor: Colors.red,
+                        ),
+                      );
+                    } else {
+                      // Navigate to the next screen
+                      Navigator.push(
+                        context,
+                        MaterialPageRoute(
+                          builder: (context) => SecondSignScreen(
+                            confpassword: _confirmPasswordController.text,
+                            name: _nameController.text,
+                            email: _emailController.text,
+                            password: _passwordController.text,
+                            parentName: _parentNameController.text,
+                            parentPhone: _parentPhoneController.text,
+                          ),
+                        ),
+                      );
+                    }
                   },
                   style: ElevatedButton.styleFrom(
                     backgroundColor: redcolor,
-                    padding: EdgeInsets.symmetric(horizontal: 50, vertical: 15),
-                    textStyle: TextStyle(fontSize: 18),
+                    padding: const EdgeInsets.symmetric(
+                        horizontal: 50, vertical: 15),
+                    textStyle: const TextStyle(fontSize: 18),
                     shape: RoundedRectangleBorder(
                       borderRadius: BorderRadius.circular(10),
                     ),
                   ),
-                  child: Text(
+                  child: const Text(
                     'اكمل',
                     style: TextStyle(color: Colors.white),
                   ),
                 ),
               ),
-              SizedBox(height: 30),
-              Divider(color: Colors.grey),
-              SizedBox(height: 30),
+              const SizedBox(height: 30),
+              const Divider(color: Colors.grey),
+              const SizedBox(height: 30),
               Row(
                 mainAxisAlignment: MainAxisAlignment.center,
                 children: [
                   buildSocialIconButton(FontAwesomeIcons.facebook),
-                  SizedBox(width: 20),
+                  const SizedBox(width: 20),
                   buildSocialIconButton(FontAwesomeIcons.instagram),
-                  SizedBox(width: 20),
+                  const SizedBox(width: 20),
                   buildSocialIconButton(FontAwesomeIcons.twitter),
                 ],
               ),
-              SizedBox(height: 30),
+              const SizedBox(height: 30),
               Center(
                 child: TextButton(
                   onPressed: () {
                     Navigator.of(context).pop();
                   },
-                  child: Text(
+                  child: const Text(
                     'لديك حساب؟ تسجيل الدخول',
                     style: TextStyle(fontSize: 18, color: redcolor),
                   ),
@@ -139,43 +173,58 @@ class SignScreen extends StatelessWidget {
       decoration: InputDecoration(
         labelText: labelText,
         prefixIcon: prefixIcon,
-        border: UnderlineInputBorder(),
-        focusedBorder: UnderlineInputBorder(
-          borderSide: BorderSide(color: redcolor),
+        border: OutlineInputBorder(
+          borderRadius: BorderRadius.circular(30.0),
         ),
-        enabledBorder: UnderlineInputBorder(
-          borderSide: BorderSide(color: redcolor),
+        focusedBorder: OutlineInputBorder(
+          borderSide: const BorderSide(color: redcolor),
+          borderRadius: BorderRadius.circular(30.0),
+        ),
+        enabledBorder: OutlineInputBorder(
+          borderSide: const BorderSide(color: redcolor),
+          borderRadius: BorderRadius.circular(30.0),
         ),
       ),
     );
   }
 
-  Widget buildPasswordField() {
+  Widget buildPasswordField(
+      TextEditingController controller, String labelText) {
     bool _passwordVisible = false;
 
-    return TextField(
-      controller: _passwordController,
-      obscureText: !_passwordVisible,
-      decoration: InputDecoration(
-        labelText: 'الرقم السري',
-        prefixIcon: Icon(Icons.lock, color: redcolor),
-        suffixIcon: IconButton(
-          icon: Icon(
-            _passwordVisible ? Icons.visibility : Icons.visibility_off,
-            color: redcolor,
+    return StatefulBuilder(
+      builder: (context, setState) {
+        return TextField(
+          controller: controller,
+          obscureText: !_passwordVisible,
+          decoration: InputDecoration(
+            labelText: labelText,
+            prefixIcon: const Icon(Icons.lock, color: redcolor),
+            suffixIcon: IconButton(
+              icon: Icon(
+                _passwordVisible ? Icons.visibility : Icons.visibility_off,
+                color: redcolor,
+              ),
+              onPressed: () {
+                setState(() {
+                  _passwordVisible = !_passwordVisible;
+                });
+              },
+            ),
+            border: OutlineInputBorder(
+              borderRadius: BorderRadius.circular(30.0),
+            ),
+            focusedBorder: OutlineInputBorder(
+              borderSide: const BorderSide(color: redcolor),
+              borderRadius: BorderRadius.circular(30.0),
+            ),
+            enabledBorder: OutlineInputBorder(
+              borderSide: const BorderSide(color: redcolor),
+              borderRadius: BorderRadius.circular(30.0),
+            ),
           ),
-          onPressed: () {
-            _passwordVisible = !_passwordVisible;
-          },
-        ),
-        border: UnderlineInputBorder(),
-        focusedBorder: UnderlineInputBorder(
-          borderSide: BorderSide(color: redcolor),
-        ),
-        enabledBorder: UnderlineInputBorder(
-          borderSide: BorderSide(color: redcolor),
-        ),
-      ),
+        );
+      },
     );
   }
 
