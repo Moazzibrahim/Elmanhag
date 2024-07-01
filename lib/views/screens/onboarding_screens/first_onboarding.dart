37ab06293e50d912d3a64b75e493a2886ce5b02c
Moaz Ibrahim <98399366+wezza99@users.noreply.github.com>
2024-07-01 14:31:34 +0300

    lol

--- lib/views/screens/onboarding_screens/first_onboarding.dart
+++ lib/views/screens/onboarding_screens/first_onboarding.dart
@@ -1,3 +1,5 @@
+// ignore_for_file: use_build_context_synchronously
+
 import 'package:flutter/material.dart';
 import 'package:flutter_application_1/constants/colors.dart';
 import 'package:flutter_application_1/views/screens/onboarding_screens/second_onboarding.dart';
