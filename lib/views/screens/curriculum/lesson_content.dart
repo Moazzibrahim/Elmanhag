091bfe61d22551946ab098bda1bd52a0636e2b19
Moaz Ibrahim <98399366+wezza99@users.noreply.github.com>
2024-07-01 16:05:10 +0300

    video screen done

---
+++ lib/views/screens/curriculum/lesson_content.dart
@@ -0,0 +1,198 @@
+import 'package:flutter/material.dart';
+import 'package:video_player/video_player.dart';
+import 'package:flutter/services.dart';
+import 'package:webview_flutter_x5/webview_flutter.dart'; // Add this import for SystemChrome
+
+class IdeasContent extends StatefulWidget {
+  const IdeasContent({
+    super.key,
+  });
+  //final Lesson lesson;
+
+  @override
+  State<IdeasContent> createState() => _IdeasContentState();
+}
+
+class _IdeasContentState extends State<IdeasContent> {
+  late VideoPlayerController controller;
+  int rating = 0;
+  int viewedVideoIndex = 0;
+  @override
+  void initState() {
+    super.initState();
+    WebView.platform;
+    SystemChrome.setPreferredOrientations([
+      DeviceOrientation.landscapeRight,
+      DeviceOrientation.landscapeLeft,
+    ]);
+  }
+
+  @override
+  void dispose() {
+    SystemChrome.setPreferredOrientations([
+      DeviceOrientation.portraitUp,
+      DeviceOrientation.portraitDown,
+    ]);
+    controller.dispose();
+    super.dispose();
+  }
+
+  void updateRating(int newRating) {
+    setState(() {
+      rating = newRating;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.all(8.0),
+      child: SingleChildScrollView(
+        child: Column(
+          children: [
+            const AspectRatio(
+              aspectRatio: 1 / 1,
+              child: WebView(
+                initialUrl:
+                    "https://ucloud.mfscripts.com/video/embed/4g/640x320/Iron_Sky_Trailer.mp4", //widget.lesson.videos[viewedVideoIndex].videoLink,
+                javascriptMode: JavascriptMode.unrestricted,
+                initialMediaPlaybackPolicy:
+                    AutoMediaPlaybackPolicy.always_allow,
+              ),
+            ),
+            const SizedBox(
+              height: 10,
+            ),
+            const Row(
+              children: [
+                SizedBox(
+                  width: 20,
+                ),
+                Text(' الوحده الاولى الدرس الثانى '),
+                SizedBox(
+                  width: 10,
+                ),
+              ],
+            ),
+            const SizedBox(
+              height: 15,
+            ),
+            const Row(
+              children: [
+                SizedBox(
+                  width: 20,
+                ),
+                CircleAvatar(
+                  backgroundImage: AssetImage('assets/images/logo.png'),
+                ),
+                SizedBox(
+                  width: 10,
+                ),
+                Text('مستر احمد الباشا'),
+              ],
+            ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.end,
+              children: [
+                for (int i = 0; i < 5; i++)
+                  GestureDetector(
+                    onTap: () {
+                      updateRating(i);
+                    },
+                    child: Icon(
+                      i <= rating ? Icons.star : Icons.star_border_outlined,
+                      color: Colors.redAccent[700],
+                    ),
+                  ),
+                const SizedBox(
+                  width: 5,
+                ),
+                Container(
+                  padding: const EdgeInsets.all(8),
+                  decoration: BoxDecoration(
+                      borderRadius: BorderRadius.circular(10),
+                      color: const Color.fromARGB(255, 194, 193, 193)),
+                  child: const Row(
+                    children: [
+                      Text('Report'),
+                      Icon(Icons.flag_outlined),
+                    ],
+                  ),
+                ),
+              ],
+            ),
+            //widget.lesson.videos.isNotEmpty
+            ListView.builder(
+              physics: const NeverScrollableScrollPhysics(),
+              shrinkWrap: true,
+              itemCount: 2, //widget.lesson.videos.length -1,
+              itemBuilder: (context, i) {
+                int index = i + 1;
+                return Row(
+                  children: [
+                    GestureDetector(
+                      onTap: () {
+                        setState(() {
+                          viewedVideoIndex = index;
+                        });
+                      },
+                      child: Container(
+                        height: 150,
+                        width: 200,
+                        margin: const EdgeInsets.symmetric(vertical: 8),
+                        padding: const EdgeInsets.all(8),
+                        decoration: BoxDecoration(
+                          borderRadius: BorderRadius.circular(12),
+                          color: const Color.fromARGB(255, 55, 54, 54),
+                        ),
+                        child: Column(
+                          mainAxisAlignment: MainAxisAlignment.end,
+                          children: [
+                            Row(
+                              mainAxisAlignment: MainAxisAlignment.start,
+                              children: [
+                                Container(
+                                  padding: const EdgeInsets.all(5),
+                                  decoration: BoxDecoration(
+                                    borderRadius: BorderRadius.circular(16),
+                                    color: const Color.fromARGB(
+                                        255, 195, 194, 194),
+                                  ),
+                                  child: const Text('2:30'),
+                                ),
+                              ],
+                            )
+                          ],
+                        ),
+                      ),
+                    ),
+                    const SizedBox(
+                      width: 10,
+                    ),
+                    const Expanded(
+                      child: Column(
+                        children: [
+                          Row(
+                            mainAxisAlignment: MainAxisAlignment.start,
+                            children: [
+                              Text(//widget.lesson.videos[index].videoName ??
+                                  'مستر احمد الباشا'),
+                            ],
+                          ),
+                          SizedBox(
+                            height: 10,
+                          ),
+                          Text('الوحده الاولى الدرس الثالث')
+                        ],
+                      ),
+                    )
+                  ],
+                );
+              },
+            )
+          ],
+        ),
+      ),
+    );
+  }
+}
---
+++ lib/views/screens/curriculum/video_screen.dart
@@ -0,0 +1,93 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_application_1/views/screens/curriculum/lesson_content.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+
+class LessonsVideos extends StatefulWidget {
+  const LessonsVideos({
+    super.key,
+  });
+  //final Lesson lesson;
+
+  @override
+  State<LessonsVideos> createState() => _LessonsVideosState();
+}
+
+class _LessonsVideosState extends State<LessonsVideos> {
+  @override
+  Widget build(BuildContext context) {
+    return DefaultTabController(
+      length: 2,
+      initialIndex: 0,
+      child: Scaffold(
+        appBar: AppBar(
+          title: const Text("lesson 1"),
+          leading: Container(
+            margin: const EdgeInsets.all(6),
+            decoration: BoxDecoration(
+                color: const Color.fromRGBO(250, 226, 229, 1),
+                borderRadius: BorderRadius.circular(12)),
+            child: IconButton(
+              onPressed: () {
+                Navigator.of(context).pop();
+              },
+              icon: Icon(
+                Icons.arrow_back,
+                color: Colors.redAccent[700],
+              ),
+            ),
+          ),
+        ),
+        body: Padding(
+          padding: const EdgeInsets.all(8.0),
+          child: Column(
+            children: [
+              Container(
+                height: 50,
+                decoration: BoxDecoration(
+                  borderRadius: BorderRadius.circular(10),
+                ),
+                child: TabBar(
+                  labelPadding:
+                      EdgeInsets.zero, // No padding between label and indicator
+                  indicator: BoxDecoration(
+                    color: Colors.redAccent[700],
+                    borderRadius: BorderRadius.circular(10.r),
+                  ),
+                  labelColor: Colors.white,
+                  unselectedLabelColor: Colors.redAccent[700],
+                  tabs: const [
+                    _CustomTab(text: 'فيديوهات'),
+                    _CustomTab(text: 'واجبات'),
+                  ],
+                ),
+              ),
+              const Expanded(
+                child: TabBarView(children: [
+                  IdeasContent(), //lesson: widget.lesson,),// replace this text with ideas Widgets
+                ]),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
+
+class _CustomTab extends StatelessWidget {
+  final String text;
+
+  const _CustomTab({required this.text});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      height: 50.h,
+      alignment: Alignment.center,
+      child: Text(
+        text,
+        style: const TextStyle(fontSize: 16),
+      ),
+    );
+  }
+}
