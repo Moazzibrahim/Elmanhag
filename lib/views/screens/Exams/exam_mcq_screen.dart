106f5b5671daf61188426af1d9b2100537dc116f
Ahmed-Ibrahim618 <ahmed.ibrahim61879@gmail.com>
2024-08-21 10:49:18 +0300

    exam

--- lib/views/screens/Exams/exam_mcq_screen.dart
+++ lib/views/screens/Exams/exam_mcq_screen.dart
@@ -15,81 +15,104 @@ class _ExamScreenState extends State<ExamScreen> {
   @override
   Widget build(BuildContext context) {
     final theme = Theme.of(context);
+    bool isDarkMode = theme.brightness == Brightness.dark;
+
     return Scaffold(
-      appBar: AppBar(
-        leading: IconButton(
-          icon: const Icon(
-            Icons.arrow_back_ios,
-            color: redcolor,
-          ),
-          onPressed: () {
-            // Handle back button press
-            Navigator.pop(context);
-          },
-        ),
-        title: const Center(
-          child: Text(
-            'الامتحان',
-            style: TextStyle(
-              color: redcolor,
-              fontSize: 28,
-            ),
-          ),
-        ),
-        backgroundColor: theme.scaffoldBackgroundColor,
-        elevation: 0,
-      ),
-      body: Padding(
-        padding: const EdgeInsets.all(16.0),
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.end,
-          children: [
-            const Text(
-              'اختر الاجابه الصحيحه',
-              style: TextStyle(
-                  color: Colors.grey,
-                  fontSize: 16,
-                  fontWeight: FontWeight.w500),
-            ),
-            const SizedBox(height: 8),
-            const Text(
-              'ما هو القاسم المشترك الأكبر للعددين 12 و 18؟',
-              style: TextStyle(
-                  fontSize: 18, fontWeight: FontWeight.w600, color: greycolor),
-            ),
-            const SizedBox(height: 16),
-            _buildRadioOption('أ) 3', '3'),
-            _buildRadioOption('ب) 6', '6'),
-            _buildRadioOption('ج) 9', '9'),
-            _buildRadioOption('د) 12', '12'),
-            const Spacer(),
-            Row(
-              mainAxisAlignment: MainAxisAlignment.start,
-              children: [
-                ElevatedButton(
-                  style: ElevatedButton.styleFrom(
-                    backgroundColor: redcolor,
-                    padding: const EdgeInsets.symmetric(
-                        horizontal: 30, vertical: 10),
-                    shape: RoundedRectangleBorder(
-                      borderRadius: BorderRadius.circular(10),
-                    ),
-                  ),
-                  onPressed: () {
-                    Navigator.of(context).push(MaterialPageRoute(
-                        builder: (context) => const ExamScreenWriteOrWrong()));
-                  },
-                  child: const Text(
-                    'التالي',
-                    style: TextStyle(
-                      fontSize: 16,
-                      color: Colors.white,
-                    ),
-                  ),
+      body: Container(
+        decoration: isDarkMode
+            ? const BoxDecoration(
+                image: DecorationImage(
+                  image: AssetImage('assets/images/Ellipse 198.png'),
+                  fit: BoxFit.cover,
                 ),
-              ],
-            ),
-          ],
+              )
+            : null,
+        child: Padding(
+          padding: const EdgeInsets.all(16.0),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.end,
+            children: [
+              SizedBox(
+                height: 25,
+              ),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  IconButton(
+                    icon: const Icon(
+                      Icons.arrow_back_ios,
+                      color: redcolor,
+                    ),
+                    onPressed: () {
+                      Navigator.pop(context);
+                    },
+                  ),
+                  Spacer(
+                    flex: 2,
+                  ),
+                  const Text(
+                    'الامتحان',
+                    style: TextStyle(
+                      color: redcolor,
+                      fontSize: 28,
+                    ),
+                  ),
+                  Spacer(
+                    flex: 3,
+                  )
+                ],
+              ),
+              const SizedBox(height: 16),
+              const Text(
+                'اختر الاجابه الصحيحه',
+                style: TextStyle(
+                    color: Colors.grey,
+                    fontSize: 16,
+                    fontWeight: FontWeight.w500),
+              ),
+              const SizedBox(height: 8),
+              const Text(
+                'ما هو القاسم المشترك الأكبر للعددين 12 و 18؟',
+                style: TextStyle(
+                    fontSize: 18,
+                    fontWeight: FontWeight.w600,
+                    color: greycolor),
+              ),
+              const SizedBox(height: 16),
+              _buildRadioOption('أ) 3', '3'),
+              _buildRadioOption('ب) 6', '6'),
+              _buildRadioOption('ج) 9', '9'),
+              _buildRadioOption('د) 12', '12'),
+              const Spacer(),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.start,
+                children: [
+                  ElevatedButton(
+                    style: ElevatedButton.styleFrom(
+                      backgroundColor: redcolor,
+                      padding: const EdgeInsets.symmetric(
+                          horizontal: 30, vertical: 10),
+                      shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(10),
+                      ),
+                    ),
+                    onPressed: () {
+                      Navigator.of(context).push(MaterialPageRoute(
+                          builder: (context) =>
+                              const ExamScreenWriteOrWrong()));
+                    },
+                    child: const Text(
+                      'التالي',
+                      style: TextStyle(
+                        fontSize: 16,
+                        color: Colors.white,
+                      ),
+                    ),
+                  ),
+                ],
+              ),
+            ],
+          ),
         ),
       ),
     );
