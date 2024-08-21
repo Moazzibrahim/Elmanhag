fd98dd8003e3bdb5302713258360dd07b95fee7b
Ahmed-Ibrahim618 <ahmed.ibrahim61879@gmail.com>
2024-08-21 11:00:49 +0300

    complete

--- lib/views/screens/Exams/complete.dart
+++ lib/views/screens/Exams/complete.dart
@@ -99,112 +99,130 @@ class Complete extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    final theme = Theme.of(context);
+    bool isDarkMode = theme.brightness == Brightness.dark;
+
     return Scaffold(
-      appBar: AppBar(
-        backgroundColor: Colors.transparent,
-        elevation: 0,
-        leading: IconButton(
-          icon: const Icon(Icons.arrow_back_ios, color: redcolor),
-          onPressed: () {
-            // Handle back button press
-          },
-        ),
-        title: const Text(
-          'الامتحان',
-          style: TextStyle(
-            color: redcolor,
-            fontSize: 24,
-            fontWeight: FontWeight.bold,
-          ),
-        ),
-        centerTitle: true,
-      ),
-      body: Padding(
-        padding: const EdgeInsets.symmetric(horizontal: 20.0),
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.center,
-          children: [
-            const SizedBox(height: 20),
-            const Text(
-              'اختر الاجابه الصحيحه من بين',
-              style: TextStyle(
-                fontSize: 18,
-                fontWeight: FontWeight.w500,
-              ),
-              textAlign: TextAlign.center,
-            ),
-            const Text(
-              'الكلمات التاليه',
-              style: TextStyle(
-                fontSize: 18,
-                fontWeight: FontWeight.w500,
-              ),
-              textAlign: TextAlign.center,
-            ),
-            const SizedBox(height: 10),
-            const Text(
-              '(الارنــــب _ الفيــــل _ الفأر_ السلحفاء)',
-              style: TextStyle(
-                fontSize: 18,
-                fontWeight: FontWeight.w500,
-              ),
-              textAlign: TextAlign.center,
-            ),
-            const SizedBox(height: 30),
-            const Text(
-              'حيوان الـــــــ _________ اسرع من _______',
-              style: TextStyle(
-                fontSize: 18,
-              ),
-              textAlign: TextAlign.center,
-            ),
-            const SizedBox(height: 20),
-            const Text(
-              'يخاف حيوان _______ من حيوان _______',
-              style: TextStyle(
-                fontSize: 18,
-              ),
-              textAlign: TextAlign.center,
-            ),
-            const Spacer(),
-            Row(
-              mainAxisAlignment: MainAxisAlignment.spaceBetween,
-              children: [
-                ElevatedButton(
-                  onPressed: () {
-                    // Handle next button press
-                    showResultDialog(context);
-                  },
-                  style: ElevatedButton.styleFrom(
-                    backgroundColor: redcolor,
-                    padding: const EdgeInsets.symmetric(
-                        horizontal: 30, vertical: 10),
-                    shape: RoundedRectangleBorder(
-                      borderRadius: BorderRadius.circular(10),
-                    ),
-                  ),
-                  child: const Text(
-                    'التالي',
-                    style: TextStyle(fontSize: 18, color: Colors.white),
-                  ),
+      body: Container(
+        decoration: isDarkMode
+            ? const BoxDecoration(
+                image: DecorationImage(
+                  image: AssetImage('assets/images/Ellipse 198.png'),
+                  fit: BoxFit.cover,
                 ),
-                TextButton(
-                  onPressed: () {
-                    // Handle previous button press
-                    Navigator.pop(context);
-                  },
-                  child: const Text(
-                    'السابق',
+              )
+            : null,
+        child: Padding(
+          padding: const EdgeInsets.symmetric(horizontal: 20.0),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: [
+              const SizedBox(height: 25),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  IconButton(
+                    icon: const Icon(Icons.arrow_back_ios, color: redcolor),
+                    onPressed: () {
+                      Navigator.pop(context);
+                    },
+                  ),
+                  const Spacer(
+                    flex: 2,
+                  ),
+                  const Text(
+                    'الامتحان',
                     style: TextStyle(
-                      color: Colors.red,
-                      fontSize: 18,
+                      color: redcolor,
+                      fontSize: 24,
+                      fontWeight: FontWeight.bold,
                     ),
                   ),
+                  const Spacer(
+                    flex: 3,
+                  )
+                ],
+              ),
+              const SizedBox(height: 20),
+              const Text(
+                'اختر الاجابه الصحيحه من بين',
+                style: TextStyle(
+                  fontSize: 18,
+                  fontWeight: FontWeight.w500,
                 ),
-              ],
-            ),
-            const SizedBox(height: 20),
-          ],
+                textAlign: TextAlign.center,
+              ),
+              const Text(
+                'الكلمات التاليه',
+                style: TextStyle(
+                  fontSize: 18,
+                  fontWeight: FontWeight.w500,
+                ),
+                textAlign: TextAlign.center,
+              ),
+              const SizedBox(height: 10),
+              const Text(
+                '(الارنــــب _ الفيــــل _ الفأر_ السلحفاء)',
+                style: TextStyle(
+                  fontSize: 18,
+                  fontWeight: FontWeight.w500,
+                ),
+                textAlign: TextAlign.center,
+              ),
+              const SizedBox(height: 30),
+              const Text(
+                'حيوان الـــــــ _________ اسرع من _______',
+                style: TextStyle(
+                  fontSize: 18,
+                ),
+                textAlign: TextAlign.center,
+              ),
+              const SizedBox(height: 20),
+              const Text(
+                'يخاف حيوان _______ من حيوان _______',
+                style: TextStyle(
+                  fontSize: 18,
+                ),
+                textAlign: TextAlign.center,
+              ),
+              const Spacer(),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  ElevatedButton(
+                    onPressed: () {
+                      showResultDialog(context);
+                    },
+                    style: ElevatedButton.styleFrom(
+                      backgroundColor: redcolor,
+                      padding: const EdgeInsets.symmetric(
+                          horizontal: 30, vertical: 10),
+                      shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(10),
+                      ),
+                    ),
+                    child: const Text(
+                      'التالي',
+                      style: TextStyle(fontSize: 18, color: Colors.white),
+                    ),
+                  ),
+                  TextButton(
+                    onPressed: () {
+                      Navigator.pop(context);
+                    },
+                    child: const Text(
+                      'السابق',
+                      style: TextStyle(
+                        color: redcolor,
+                        fontSize: 18,
+                      ),
+                    ),
+                  ),
+                ],
+              ),
+              const SizedBox(height: 20),
+            ],
+          ),
         ),
       ),
     );
