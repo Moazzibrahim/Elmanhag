944a761680f98c36fcb972369bcebdff09e594b3
Moaz Ibrahim <98399366+wezza99@users.noreply.github.com>
2024-08-25 13:05:33 +0300

    dd

---
+++ lib/views/widgets/hw_levels_widget.dart
@@ -0,0 +1,147 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_application_1/constants/colors.dart';
+import 'package:flutter_application_1/views/screens/homework/hw_mcq_screen.dart';
+
+class HomeWorkWidget extends StatefulWidget {
+  const HomeWorkWidget({super.key});
+
+  @override
+  HomeWorkWidgetState createState() => HomeWorkWidgetState();
+}
+
+class HomeWorkWidgetState extends State<HomeWorkWidget> {
+  // List for 3 tasks, each initialized with 0 stars
+  List<int> taskStars = List.generate(3, (index) => 0);
+  int unlockedTasks = 1; // Only the first task is initially unlocked
+
+  @override
+  Widget build(BuildContext context) {
+    final theme = Theme.of(context);
+    bool isDarkMode = theme.brightness == Brightness.dark;
+    return Container(
+      decoration: isDarkMode
+          ? const BoxDecoration(
+              image: DecorationImage(
+                image: AssetImage('assets/images/Ellipse 198.png'),
+                fit: BoxFit.cover,
+              ),
+            )
+          : null,
+      child: Padding(
+        padding: const EdgeInsets.all(16.0),
+        child: Column(
+          children: [
+            const SizedBox(height: 20),
+            Expanded(
+              child: GridView.builder(
+                gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+                  crossAxisCount: 3,
+                  childAspectRatio: 1,
+                  crossAxisSpacing: 10,
+                  mainAxisSpacing: 10,
+                ),
+                itemCount: taskStars.length,
+                itemBuilder: (context, index) {
+                  if (index < unlockedTasks) {
+                    return TaskCard(
+                      index: index + 1,
+                      stars: taskStars[index],
+                      onTap: () {
+                        Navigator.push(
+                          context,
+                          MaterialPageRoute(
+                            builder: (context) => const HomeworkMcqScreen(),
+                          ),
+                        );
+                      },
+                    );
+                  } else {
+                    return const LockedCard();
+                  }
+                },
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+class TaskCard extends StatelessWidget {
+  final int index;
+  final int stars;
+  final VoidCallback onTap;
+
+  const TaskCard({
+    required this.index,
+    required this.stars,
+    required this.onTap,
+    super.key,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    final theme = Theme.of(context);
+    bool isDarkMode = theme.brightness == Brightness.dark;
+    return GestureDetector(
+      onTap: onTap,
+      child: Container(
+        decoration: BoxDecoration(
+          color: isDarkMode ? Colors.black : Colors.white,
+          borderRadius: BorderRadius.circular(10),
+          border:
+              Border.all(color: isDarkMode ? Colors.black : redcolor, width: 2),
+        ),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Text(
+              'Task $index', // Displaying the task number
+              style: const TextStyle(
+                fontSize: 24,
+                color: redcolor,
+                fontWeight: FontWeight.bold,
+              ),
+            ),
+            const SizedBox(height: 5),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: List.generate(
+                3,
+                (starIndex) => Icon(
+                  starIndex < stars ? Icons.star : Icons.star_border,
+                  color: redcolor,
+                  size: 24,
+                ),
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+class LockedCard extends StatelessWidget {
+  const LockedCard({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    final theme = Theme.of(context);
+    bool isDarkMode = theme.brightness == Brightness.dark;
+    return Container(
+      decoration: BoxDecoration(
+        color: isDarkMode ? Colors.black : Colors.grey.shade200,
+        borderRadius: BorderRadius.circular(10),
+      ),
+      child: const Center(
+        child: Icon(
+          Icons.lock,
+          color: Colors.grey,
+          size: 48,
+        ),
+      ),
+    );
+  }
+}
